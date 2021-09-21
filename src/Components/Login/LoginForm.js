cfdec55fcba21e2687202161f919015c1ad48363
Bruno Fukumori <brunofukumori@gmail.com>
2021-09-21 18:33:13 -0300

    adicionado assets ao projeto, criadas as rotas inicias para Home e Login e iniciada a construção do Header e página de Login

---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  async function handleSubmit(event) {
+    event.PreventDefault();
+    const response = await fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
+    const json = await response.json();
+    return json;
+  }
+
+  return (
+    <section onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <form>
+        <input type="text"
+          onChange={({ target }) => setUsername(target.value)}
+          value={username}
+        />
+        <input type="password"
+          onChange={({ target }) => setPassword(target.value)}
+          value={password}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/create">Cadastro</Link>
+    </section>
+  )
+}
+
+export default LoginForm
