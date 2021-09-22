83d742fd3583f5c9d44380d401946adde27e8c1e
Bruno Fukumori <brunofukumori@gmail.com>
2021-09-21 22:39:37 -0300

    criado contextos para as informações de login e refatorado a api

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,23 +3,17 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../Contexts/UserContext';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
     if (username.validate() && password.validate()) {
-      const response = await fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify()
-      });
-      const json = await response.json();
-      return json;
+      userLogin(username.value, password.value);
     }
   }
 
