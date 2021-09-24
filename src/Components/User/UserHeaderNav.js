197b42202ca02fe0175197e664f233103af6dce4
Bruno Fukumori <brunofukumori@gmail.com>
2021-09-23 21:42:09 -0300

    Aula 0818 Mobile Menu

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -6,29 +6,41 @@ import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
+import { useLocation } from 'react-router';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia('(max-width: 40rem');
+  const [mobileMenu, SetmobileMenu] = React.useState(false);
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    SetmobileMenu(false)
+  }, [pathname])
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/account" className={({ isActive }) =>
-        `${isActive ? styles.active : ""}`} end>
-        <MinhasFotos />{mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink to="/account/post" className={({ isActive }) =>
-        `${isActive ? styles.active : ""}`}>
-        <AdicionarFoto />{mobile && 'Adicionar Fotos'}
-      </NavLink>
-      <NavLink to="/account/stats" className={({ isActive }) =>
-        `${isActive ? styles.active : ""}`}>
-        <Estatisticas />{mobile && 'Estatísticas'}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Sair />{mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && <button aria-label="Menu" onClick={() => SetmobileMenu(!mobileMenu)} className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}></button>}
+
+      <nav className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}>
+        <NavLink to="/account" className={({ isActive }) =>
+          `${isActive ? styles.active : ""}`} end>
+          <MinhasFotos />{mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/account/post" className={({ isActive }) =>
+          `${isActive ? styles.active : ""}`}>
+          <AdicionarFoto />{mobile && 'Adicionar Fotos'}
+        </NavLink>
+        <NavLink to="/account/stats" className={({ isActive }) =>
+          `${isActive ? styles.active : ""}`}>
+          <Estatisticas />{mobile && 'Estatísticas'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />{mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   )
 }
 
---
+++ src/Hooks/useMedia.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const useMedia = (media) => {
+  const [match, Setmatch] = React.useState(null);
+  React.useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      Setmatch(matches);
+    }
+    changeMatch();
+    window.addEventListener('resize', changeMatch);
+    return () => {
+      window.removeEventListener('resize', changeMatch);
+    }
+  }, [media]);
+
+  return match;
+}
+
+export default useMedia;
