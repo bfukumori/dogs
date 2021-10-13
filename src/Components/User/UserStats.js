f93b8c2bab31aa0e10c712277b809de66bf05fc4
Bruno Fukumori <brunofukumori@gmail.com>
2021-10-12 22:56:06 -0300

    Aulas 0835 0836 Victory e Lazy/Suspense

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import { STATS_GET } from '../../Api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  )
+  const { data, error, loading, request } = useFetch();
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request])
+
+  if (loading) return <Loading />
+  if (error) return <Error />
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    )
+  else return null
 };
 
 export default UserStats;
