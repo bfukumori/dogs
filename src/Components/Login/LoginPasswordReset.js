cb5f5022cee04851b62e223c850a5a2ff2138e7b
Bruno Fukumori <brunofukumori@gmail.com>
2021-10-12 22:14:36 -0300

    Aulas 0833 e 0834 Head e Footer

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -6,6 +6,7 @@ import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
 import Error from '../Helper/Error';
+import Head from '../Helper/Head';
 
 const LoginPasswordReset = () => {
   const [login, setLogin] = React.useState('');
@@ -32,6 +33,7 @@ const LoginPasswordReset = () => {
   }
 
   return <div>
+    <Head title="Resete a senha?" />
     <h1 className="title" >Resete a Senha</h1>
     <form onSubmit={handleSubmit}>
       <Input label="Nova Senha" type="password" name="password" {...password} />
